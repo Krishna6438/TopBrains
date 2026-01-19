03425dfa29be3a5e58210d2abcef9ce38718d012
Krishna Sharma <Krishnasharma283125@gmail.com>
2026-01-19 12:10:07 +0530

    String format

---
+++ StringFormat.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+public record Student(string Name, int Marks);
+
+public class StringFormatter
+{
+    // public static string BuildAndSerialize(string[] items, int minScore)
+    // {
+        
+    // }
+}
